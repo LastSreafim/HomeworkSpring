3f518047a343f983e14056bf2e04963465751ef2
Nazar <epoxasmute@mail.ru>
2024-05-10 22:38:10 +0300

    Git init

---
+++ src/main/java/com/maybehomework/skyprotest4homework/SkyproTest4homeworkApplication.java
@@ -0,0 +1,13 @@
+package com.maybehomework.skyprotest4homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproTest4homeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproTest4homeworkApplication.class, args);
+	}
+
+}
